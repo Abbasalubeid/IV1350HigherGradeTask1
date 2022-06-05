2a3e99381988494b932cea473ee7cc446c9aebd7
Abbasalubeid <91632863+Abbasalubeid@users.noreply.github.com>
2022-06-05 18:24:15 +0200

    Update View.java

--- src/se/kth/iv1350/POS/view/View.java
+++ src/se/kth/iv1350/POS/view/View.java
@@ -45,32 +45,16 @@ public class View {
 		String barcodeForKetchup = "k1020k";
 
 		contr.startSale();
-		System.out.println("A new sale has been started" + "\n");
 		contr.scanItem(barcodeForTomato);
 		contr.scanItem(barcodeForChips);
+		contr.scanItem(barcodeForKetchup);
 		contr.scanItem(barcodeForKetchup);
 		contr.scanItem(barcodeForTomato);
 		System.out.println("Items has been scanned" + "\n");
 		totalPrice = contr.getTotalPrice();
 		System.out.println("The price is: " + totalPrice + "\n");
-		System.out.println("The customer with id 662 want a discount based on his age: " + "\n");
-		try {
-			contr.calculatePriceAfterDiscount("age", contr.getAccountingSystem().getCustomer("662"));
-		} catch (DiscountException exc) {
-			System.out.print("User information: Customer with the id " + exc.getCustomerID() + " "
-					+ "is not eligible for a discount!"+ "\n");
-			System.out.print("LOG: The customer does not fit in any of the implemented algorithms"
-					+ "for calculating a discount"+ "\n" + "\n");
 
-		} catch (InstantiationException exc) {
-			System.out.print("User information: There is no discount of the type you searched for, "
-					+ "please try again!" + "\n");
-			System.out.print("LOG: There is no discount calculator implementation that has this "
-					+ "description" + "\n" + "\n");
-		}
-		totalPrice = contr.getTotalPrice();
-		System.out.println("The price after discount is: " + totalPrice + "\n");
-		change = contr.payment(100);
+		change = contr.payment(200);
 		paidAmount = contr.getSale().getPaidAmount();
 		System.out.println("Customer payed: " + paidAmount + "\n");
 		System.out.println("The change of this sale is: " + change + "\n");
@@ -79,36 +63,3 @@ public class View {
 
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
